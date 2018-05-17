f517d4c4ce0d60cbcb2e080bbd9e8c9779857d1b
Mattias Andersson <mattias.andersson@aftonbladet.se>
2018-05-17 16:59:42 +0200

    First part of tablerow

--- src/components/calculateresult.js
+++ src/components/calculateresult.js
@@ -1,53 +1,54 @@
 
 
-
-countries: [ "Ryssland", "Sweden", "Germany" ]
+const countries = ["Ryssland", "Sweden", "Germany"]
 
 export const calculateResult = (games) => {
-this.state.countries.map((country) => {
-  let playedGames = 0
-  let wins = 0
-  let draws = 0
-  let losts = 0
-  let totOwnScore = 0
-  let totAgainstScore = 0
-  let points = 0
+  const result = countries.map((country) => {
+    let playedGames = 0
+    let wins = 0
+    let draws = 0
+    let losts = 0
+    let totOwnScore = 0
+    let totAgainstScore = 0
+    let points = 0
 
-  {this.props.games.map((countryTable) => {
-
-    if (country === games.homeTeam) {
-      playedGames += 1
-      totOwnScore += games.homeScore
-      totAgainstScore += games.awayScore
-        if (games.homeScore > games.awayScore) {
-          points += 3
-          wins += 1
-        } else if (games.homeScore === games.awayScore) {
-          points += 1
-          draws += 1
-        } else {
-          losts += 1
-        }
-
-    } else if ( country === games.awayTeam ) {
+    games.map((game) => {
+      if (country === game.homeTeam) {
         playedGames += 1
-        totOwnScore += games.awayScore
-        totAgainstScore += games.homeScore
-          if (games.awayScore > games.homeScore) {
+        totOwnScore += game.homeScore
+        totAgainstScore += game.awayScore
+          if (game.homeScore > game.awayScore) {
             points += 3
             wins += 1
-          } else if (games.homeScore === games.awayScore) {
+          } else if (game.homeScore === game.awayScore) {
             points += 1
             draws += 1
           } else {
             losts += 1
           }
-      }
-    })}
+          console.log(game.playedGames)
+      } else if ( country === game.awayTeam ) {
+          playedGames += 1
+          totOwnScore += game.awayScore
+          totAgainstScore += game.homeScore
+            if (game.awayScore > game.homeScore) {
+              points += 3
+              wins += 1
+            } else if (game.homeScore === game.awayScore) {
+              points += 1
+              draws += 1
+            } else {
+              losts += 1
+            }
+        }
+      })
 
-    return {country: country, playedGames: playedGames,
-      wins: wins, draws: draws, losts: losts,
-      totOwnScore: totOwnScore, totAgainstScore: totAgainstScore,
-      points: points}
+      return {country: country, playedGames: playedGames,
+        wins: wins, draws: draws, losts: losts,
+        totOwnScore: totOwnScore, totAgainstScore: totAgainstScore,
+        points: points}
 
-  })}
+    })
+  console.log(result)
+  return result
+}
--- src/components/group.js
+++ src/components/group.js
@@ -4,7 +4,7 @@ import Game from "./game"
 class Group extends React.Component {
 
   render() {
-console.log(this.props)
+// console.log(this.props)
     return (
       <div>
         { this.props.games.map((gameData) => {
